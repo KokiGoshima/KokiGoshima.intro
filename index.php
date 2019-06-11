ac681c7ca374855ac609e15f016b7fdc31e1cdd4
Koki Goshima <gosh.namnam0705@gmail.com>
2019-06-11 09:57:12 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,513 @@
+<!DOCTYPE html>
+<!--[if lt IE 8 ]><html class="no-js ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="no-js ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 8)|!(IE)]><!--><html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+
+   <!--- Basic Page Needs
+   ================================================== -->
+   <meta charset="utf-8">
+	<title>Ceevee - Free Responsive HTML5/CSS3 Template</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+   <!-- Mobile Specific Metas
+   ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- CSS
+    ================================================== -->
+   <link rel="stylesheet" href="css/default.css">
+	<link rel="stylesheet" href="css/layout.css">
+   <link rel="stylesheet" href="css/media-queries.css">
+   <link rel="stylesheet" href="css/magnific-popup.css">
+
+   <!-- Script
+   ================================================== -->
+	<script src="js/modernizr.js"></script>
+
+   <!-- Favicons
+	================================================== -->
+	<link rel="shortcut icon" href="favicon.png" >
+
+</head>
+
+<body>
+
+   <!-- Header
+   ================================================== -->
+   <header id="home">
+
+      <nav id="nav-wrap">
+
+         <a class="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+	      <a class="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
+
+         <ul id="nav" class="nav">
+            <li class="current"><a class="smoothscroll" href="#home">Home</a></li>
+            <li><a class="smoothscroll" href="#about">About</a></li>
+	         <li><a class="smoothscroll" href="#resume">Resume</a></li>
+            <li><a class="smoothscroll" href="#portfolio">Works</a></li>
+            <li><a class="smoothscroll" href="#contact">Contact</a></li>
+         </ul> <!-- end #nav -->
+
+      </nav> <!-- end #nav-wrap -->
+
+      <div class="row banner">
+         <div class="banner-text">
+            <h1 class="responsive-headline">I'm Koki Goshima.</h1>
+            <h3>I'm a Japanese based <span>pianist</span> searching for what I really wanna do from bottom of my heart. Let's <a class="smoothscroll" href="#about">start scrolling</a>
+            and learn more <a class="smoothscroll" href="#about">about me</a>.</h3>
+            <hr />
+            <ul class="social">
+               <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+               <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+               <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+               <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+            </ul>
+         </div>
+      </div>
+
+      <p class="scrolldown">
+         <a class="smoothscroll" href="#about"><i class="icon-down-circle"></i></a>
+      </p>
+
+   </header> <!-- Header End -->
+
+
+   <!-- About Section
+   ================================================== -->
+   <section id="about">
+
+      <div class="row">
+
+         <div class="three columns">
+
+            <img class="profile-pic"  src="images/koki.jpg" alt="" style="width: 250px; height: 250px;">
+
+         </div>
+
+         <div class="nine columns main-col">
+
+            <h2>About Me</h2>
+
+            <p>Place of birth: Tokyo, Setagaya-ku<br>Age: 24<br>Hobby: playing piano/ watching <a href="https://www.tv-tokyo.co.jp/wbs/" 
+               target="_blank">WBS</a>(one of Japanese news)
+               <br>Strength: <a href="http://puyo.sega.jp/portal/" target="_blank">PuyoPuyo</a>(pazzle game)<br>Experience of programming: none<br>
+            </p>
+
+            <div class="row">
+
+               <div class="columns contact-details">
+
+                  <h2>Contact Details</h2>
+                  <p class="address">
+						   <span>2-30-3, Higashitamagawa</span><br>
+						   <span>Setagaya-ku, Tokyo-to,Japan
+                     </span><br>
+						   <span>090-8434-4948</span><br>
+                     <span>gosh.namnam0705@gmail.com</span>
+					   </p>
+
+               </div>
+
+          <!--      <div class="columns download">
+                  <p>
+                     <a href="#" class="button"><i class="fa fa-download"></i>Download Resume</a>
+                  </p>
+               </div> -->
+
+            </div> <!-- end row -->
+
+         </div> <!-- end .main-col -->
+
+      </div>
+
+   </section> <!-- About Section End-->
+
+
+   <!-- Resume Section
+   ================================================== -->
+   <section id="resume">
+
+      <!-- Education
+      ----------------------------------------------- -->
+      <div class="row education">
+
+         <div class="three columns header-col">
+            <h1><span>Education</span></h1>
+         </div>
+
+         <div class="nine columns main-col">
+
+            <div class="row item">
+
+               <div class="twelve columns">
+
+                  <h3>University of Life</h3>
+                  <p class="info">Bachelor in <a href="http://global.chuo-u.ac.jp/english/" target="_blank">Chuo University</a> <span>&bull;</span> <em class="date">April 2017</em></p>
+
+                  <p>
+                     I studied about <a href="https://www.chuo-u.ac.jp/academics/faculties/letters/major/english/detail/curriculum/" target="_blank">English Linguistics</a>. My Graduation thesis is about how it is effective to shadow what native speaks for 
+                     improving pronounciation skills.
+                     
+                  </p>
+
+               </div>
+
+            </div> <!-- item end -->
+
+            <div class="row item">
+
+               <div class="twelve columns">
+
+                  <h3>Study Abroad</h3>
+                  <p class="info">Graduation from Ceb programming school <span>&bull;</span> <em class="date">March 2019</em></p>
+
+                  <p>
+                     <a href="https://nexseed.net/" target="_blank">Nexseed</a> is school for both English and programming education. I studied there for three months. Over 600  hours had been  
+                     spent for programming study.
+
+                  </p>
+
+               </div>
+
+            </div> <!-- item end -->
+
+         </div> <!-- main-col end -->
+
+      </div> <!-- End Education -->
+
+
+      <!-- Work
+      ----------------------------------------------- -->
+      <div class="row work">
+
+         <div class="three columns header-col">
+            <h1><span>Work</span></h1>
+         </div>
+
+         <div class="nine columns main-col">
+
+            <div class="row item">
+
+               <div class="twelve columns">
+
+                  <h3>Super Pianoman</h3>
+                  <!-- <p class="info">Senior UX Designer <span>&bull;</span> <em class="date">March 2010 - Present</em></p> -->
+
+                  <p>
+                  "Super" comes from my height; I am 184cm lol. I started to learn piano when I was 5 years old. I have played classical
+                  until 22. After that, Jazz is about to become my second stage in piano life. On March 31 in 2019, I won 3rd prize in contest
+                  held by <a href="http://www.piano.or.jp/" target="_blank">"Pitina"</a>
+                  </p>
+
+               </div>
+
+            </div> <!-- item end -->
+
+            <div class="row item">
+
+               <div class="twelve columns">
+
+                  <h3>Super Hotelman</h3>
+                  <!-- <p class="info">UX Designer <span>&bull;</span> <em class="date">March 2007 - February 2010</em></p> -->
+
+                  <p>
+                  <a href="https://www.newotani.co.jp/en/tokyo/" target="_blank">Hotel New Otani</a> is my first career after graduating university. My job was bell-boy and sales marketing. I have worked
+                  there for two years.
+                  </p>
+
+               </div>
+
+            </div> <!-- item end -->
+
+         </div> <!-- main-col end -->
+
+      </div> <!-- End Work -->
+
+
+      <!-- Skills
+      ----------------------------------------------- -->
+      <div class="row skill">
+
+         <div class="three columns header-col">
+            <h1><span>Skills</span></h1>
+         </div>
+
+         <div class="nine columns main-col">
+
+           
+
+				<div class="bars">
+
+				   <ul class="skills">
+					   <li><span class="bar-expand photoshop"></span><em>English</em></li>
+                  <!-- <li><span class="bar-expand illustrator"></span><em>Illustrator</em></li> -->
+						<!-- <li><span class="bar-expand wordpress"></span><em>Wordpress</em></li> -->
+						<li><span class="bar-expand css"></span><em>Piano</em></li>
+						<li><span class="bar-expand html5"></span><em>Service</em></li>
+                  <li><span class="bar-expand jquery"></span><em>PuyoPuyo</em></li>
+					</ul>
+
+				</div><!-- end skill-bars -->
+
+			</div> <!-- main-col end -->
+
+      </div> <!-- End skills -->
+
+
+   <!-- Wanna  -->
+   <div class="row wanna" style="margin-top: 50px;">
+      <div class="three columns header-col">
+            <h1><span>What I want to !</span></h1>
+         </div>
+
+         <div class="nine columns main-col">
+
+            <div class="row item">
+
+               <div class="twelve columns">
+
+                 <em>1st: Get habit of getting up early!
+                  <br>(from tomorrow)</em>
+
+               </div>
+
+            </div> <!-- item end -->
+
+            <div class="row item">
+
+               <div class="twelve columns">
+
+                  <em>2nd: Work at Venture companies!
+                     <br>(in 3 months)</em>
+
+               </div>
+
+            </div> <!-- item end -->
+
+            <div class="row item ">
+
+               <div class="twelve columns">
+
+                 <em>3rd: Work as Engineer!
+                  <br>(By 26)</em>
+
+               </div>
+
+            </div> <!-- item end -->
+
+            <div class="row item ">
+
+               <div class="twelve columns">
+
+                 <em>4th: Do Side business!
+                  <br>(By 26)</em>
+
+               </div>
+
+            </div> <!-- item end -->
+
+            <div class="row item ">
+
+               <div class="twelve columns">
+
+                 <em>5th: Connent skill of programming and that of piano!<br>(By 28)</em>
+
+               </div>
+
+            </div> <!-- item end -->
+         </div>
+
+   </div> <!-- End Wanna -->
+
+   </section> <!-- Resume Section End-->
+
+   <!-- Portfolio Section
+   ================================================== -->
+   <section id="portfolio">
+
+      <div class="row">
+         <div class="twelve columns collapsed">
+            <h1>Check Out Some of My Works.</h1>
+            <!-- portfolio-wrapper -->
+            <div id="portfolio-wrapper" class="bgrid-quarters s-bgrid-thirds cf">
+          	   <!-- <div class="columnsns portfolio-item"> -->
+                  <!-- <div class="item-wrap"> -->
+               <div class = "video1">
+                  
+                     <video controls autoplay poster="firstframe.jpg" width="320" height="240">
+                           <source src="video/IMG-1696.mp4">
+                           <!-- <source src="sample.ogv"> -->
+                           <!-- <source src="sample.webm"> -->
+                           <p>動画を再生するには、videoタグをサポートしたブラウザが必要です。</p>
+                     </video>
+                               
+               </div>
+
+               <div class = "video2">
+                  
+                     <video controls autoplay poster="firstframe.jpg" width="320" height="240">
+                           <source src="video/IMG-2551.mp4">
+                           <!-- <source src="sample.ogv">
+                           <source src="sample.webm"> -->
+                           <p>動画を再生するには、videoタグをサポートしたブラウザが必要です。</p>
+                     </video>
+                  
+               </div>
+
+               <!-- </div> -->
+            </div> 
+         </div> <!-- twelve columns end -->
+      </div>
+
+   </section>
+
+
+
+
+   <!-- Call-To-Action Section
+   ================================================== -->
+   
+
+   <!-- Testimonials Section
+   ================================================== -->
+  
+
+   <!-- Contact Section
+   ================================================== -->
+   <section id="contact">
+
+         <div class="row section-head">
+
+            <div class="two columns header-col">
+
+               <h1><span>Get In Touch.</span></h1>
+
+            </div>
+
+            <div class="ten columns">
+
+                  <p class="lead">Send me an e-mail if you have some interests to me!
+                  </p>
+
+            </div>
+
+         </div>
+
+         <div class="row">
+
+            <div class="eight columns">
+
+               <!-- form -->
+               <form action="" method="post" id="contactForm" name="contactForm">
+					<fieldset>
+
+                  <div>
+						   <label for="contactName">Name <span class="required">*</span></label>
+						   <input type="text" value="" size="35" id="contactName" name="contactName">
+                  </div>
+
+                  <div>
+						   <label for="contactEmail">Email <span class="required">*</span></label>
+						   <input type="text" value="" size="35" id="contactEmail" name="contactEmail">
+                  </div>
+
+                  <div>
+						   <label for="contactSubject">Subject</label>
+						   <input type="text" value="" size="35" id="contactSubject" name="contactSubject">
+                  </div>
+
+                  <div>
+                     <label for="contactMessage">Message <span class="required">*</span></label>
+                     <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
+                  </div>
+
+                  <div>
+                     <button class="submit">Submit</button>
+                     <span id="image-loader">
+                        <img alt="" src="images/loader.gif">
+                     </span>
+                  </div>
+
+					</fieldset>
+				   </form> <!-- Form End -->
+
+               <!-- contact-warning -->
+               <div id="message-warning"> Error boy</div>
+               <!-- contact-success -->
+				   <div id="message-success">
+                  <i class="fa fa-check"></i>Your message was sent, thank you!<br>
+				   </div>
+
+            </div>
+
+
+            <aside class="four columns footer-widgets">
+
+               <div class="widget widget_contact">
+
+					   <h4>Address and Phone</h4>
+					   <p class="address">
+					    <span>2-30-3, Higashitamagawa</span><br>
+                     <span>Setagaya-ku, Tokyo-to,Japan
+                     </span><br>
+                     <span>090-8434-4948</span><br>
+                     <span>gosh.namnam0705@gmail.com</span>
+					    </p>
+
+				   </div>
+
+               
+
+            </aside>
+
+      </div>
+
+   </section> <!-- Contact Section End-->
+
+
+   <!-- footer
+   ================================================== -->
+   <footer>
+
+      <div class="row">
+
+         <div class="twelve columns">
+
+            <ul class="social-links">
+               <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+               <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+               <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+               <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+            </ul>
+
+            <ul class="copyright">
+               <li>&copy; Copyright 2019 CeeVee</li>
+               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">KokiGoshima</a></li>   
+            </ul>
+
+         </div>
+
+         <div id="go-top"><a class="smoothscroll" title="Back to Top" href="#home"><i class="icon-up-open"></i></a></div>
+
+      </div>
+
+   </footer> <!-- Footer End-->
+
+   <!-- Java Script
+   ================================================== -->
+   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+   <script>window.jQuery || document.write('<script src="js/jquery-1.10.2.min.js"><\/script>')</script>
+   <script type="text/javascript" src="js/jquery-migrate-1.2.1.min.js"></script>
+
+   <script src="js/jquery.flexslider.js"></script>
+   <script src="js/waypoints.js"></script>
+   <script src="js/jquery.fittext.js"></script>
+   <script src="js/magnific-popup.js"></script>
+   <script src="js/init.js"></script>
+
+</body>
+
+</html>
